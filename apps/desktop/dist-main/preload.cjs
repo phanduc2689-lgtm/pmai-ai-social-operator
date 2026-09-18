4a483179aeddaba50fd51467986966f9f9b1d1a3
phanduc2689-lgtm <phanduc2689@gmail.com>
2026-09-18 22:57:27 +0700

    fix: allowlist chrome.createProfile and chrome.cloneProfile in preload

--- apps/desktop/dist-main/preload.cjs
+++ apps/desktop/dist-main/preload.cjs
@@ -7,6 +7,8 @@ const ALLOWLIST = [
   "chrome.status",
   "chrome.launch",
   "chrome.autoConnect",
+  "chrome.createProfile",
+  "chrome.cloneProfile",
   "chrome.observe",
   "chrome.goto",
   "chrome.type",
